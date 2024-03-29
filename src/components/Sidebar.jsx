a969d102909b56694a835106c29d08c861f4c5c0
Archit Kakkar <education.archit@gmail.com>
2024-03-30 02:38:21 +0530

    Implemented feature to display note name with first line of its body.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,14 +2,14 @@ import React from "react"
 
 export default function Sidebar(props) {
     // Map over the notes array to create a list of note elements
-    const noteElements = props.notes.map((note, index) => (
+    const noteElements = props.notes.map((note) => (
         <div key={note.id}>
             {/* Div for each note, with a class indicating if it's the selected note */}
             <div
                 className={`title ${note.id === props.currentNote.id ? "selected-note" : ""}`}
                 onClick={() => props.setCurrentNoteId(note.id)}
             >
-                <h4 className="text-snippet">Note {index + 1}</h4>
+                <h4 className="text-snippet">{(note.body).split('\n')[0]}</h4>
             </div>
         </div>
     ))
